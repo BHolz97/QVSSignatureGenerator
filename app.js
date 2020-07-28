d65b2c6d5bc1fad94fe110510dd79bed55cd4fcd
Brandon Holz <brndnhlz@gmail.com>
2020-07-29 00:57:03 +0200

    Generating signature

---
+++ app.js
@@ -0,0 +1,35 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/signature", function(req, res){
+    console.log(req.body.name);
+    console.log(req.body.email);
+    res.render("signature", {
+        name: req.body.name,
+        position: req.body.job1,
+        tel: req.body.tel,
+        email: req.body.email
+    });
+});
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000");
+});
