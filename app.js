debeb25e9802a2f26be0539409886df65deb7721
Brandon Holz <brndnhlz@gmail.com>
2020-07-29 01:15:43 +0200

    Prepped for deployment

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.post("/signature", function(req, res){
 
 
 
-app.listen(3000, function(){
-    console.log("Server running on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+  console.log("Server running on port 3000");
+}
+app.listen(port);
